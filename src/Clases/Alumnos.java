f596bc3bae7e2687fc54f53109154e562cf7613c
hp <hp@192.168.1.70>
2020-12-17 13:11:38 -0600

    Clase "Alumnos" getters y setters y propiedades

--- src/Clases/Alumnos.java
+++ src/Clases/Alumnos.java
@@ -10,5 +10,44 @@ package Clases;
  * @author hp
  */
 public class Alumnos {
+    private String Nombre;
+    private int id;
+
+    public Alumnos() {
+    }
+
+    public Alumnos(String Nombre, int id) {
+        this.Nombre = Nombre;
+        this.id = id;
+    }
+
+    /**
+     * @return the Nombre
+     */
+    public String getNombre() {
+        return Nombre;
+    }
+
+    /**
+     * @param Nombre the Nombre to set
+     */
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+    
     
 }
